21f87ab4078ccede9e25fa15b64440ccd10d34d7
Logan Williams <loganwilliamslw1@gmail.com>
2024-04-07 15:37:08 -0400

    batch and python scripts to do try ctgan on range of input sizes

---
+++ generator_custom/med_run.py
@@ -0,0 +1,3 @@
+from generator_ctgan import get_ctgan
+
+get_ctgan(subset_frac=0.5)
---
+++ hpc_batch_scripts/run_small_ctgan.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#BSUB -n 1
+#BSUB -W 36:00
+#BSUB -J CTGAN
+#BSUB -q gpu
+#BSUB -gpu "num=1:mode=shared:mps=no"
+#BSUB -o dnn_outputs/stdout.%J
+#BSUB -e dnn_outputs/stderr.%J
+
+source ~/.bashrc
+conda activate /usr/local/usrapps/csc522s24/lrwilli7/env_synth
+python ../generator_custom/small_run.py
+conda deactivate
